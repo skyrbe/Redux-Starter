16a0811dce3081a34b168c860ca11378ac64cea7
skyrbe <harsha.venkatram@sakhatech.com>
2017-05-31 11:01:18 +0530

    connect function, mapping state to props

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
-
+import BooksReducer  from './reducer-books';
 const rootReducer = combineReducers({
+  books : BooksReducer
 });
 
 export default rootReducer;
